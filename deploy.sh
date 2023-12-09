38c2d6399771689061ee819cae00c7a1c8032d1f
m3k01 <tmk2408@gmail.com>
2023-12-09 19:00:13 +0400

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-rm build/libs/lindaSelfGuru-0.0.1-SNAPSHOT.jar
-./gradlew bootJar
-scp ./build/libs/lindaSelfGuru-0.0.1-SNAPSHOT.jar root@62.113.100.108:backend/LindaSelfGuru/build/libs/lindaSelfGuru-0.0.1-SNAPSHOT.jar
-ssh root@62.113.100.108 -t 'cd backend/LindaSelfGuru/ && docker-compose up -d --build; bash -l'
